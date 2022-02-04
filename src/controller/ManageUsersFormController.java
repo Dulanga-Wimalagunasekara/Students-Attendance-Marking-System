cf97bdc37c6b269f224131e496feba5f31492393
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-02-04 20:54:03 +0530

    Create the manage users form

--- src/controller/ManageUsersFormController.java
+++ src/controller/ManageUsersFormController.java
@@ -8,10 +8,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import view.TM.userTM;
 
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Optional;
 
 public class ManageUsersFormController {
     public TextField txtUsername;
@@ -22,6 +20,7 @@ public class ManageUsersFormController {
     public Button btnUpdateRole;
     public TableView<userTM> tblUser;
     public PasswordField txtPassword;
+    public Button btnAddNewUser;
 
     public void initialize(){
         loadAllUsers();
@@ -41,6 +40,20 @@ public class ManageUsersFormController {
         tblUser.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("role"));
         tblUser.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("removeButton"));
 
+        tblUser.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue!=null){
+                disableControls(true);
+                txtUsername.setText(newValue.getUsername());
+                txtPassword.setText(newValue.getPassword());
+                txtConfirmPassword.setText(newValue.getPassword());
+                txtName.setText(newValue.getName());
+                menuRole.setText(newValue.getRole());
+                btnUpdateRole.setDisable(false);
+            }
+        });
+        txtPassword.setOnAction(event -> {
+            txtConfirmPassword.clear();
+        });
     }
 
     private void disableControls(boolean b) {
@@ -61,12 +74,11 @@ public class ManageUsersFormController {
             ResultSet rst = stm.executeQuery("SELECT * FROM user");
             while (rst.next()){
                 ObservableList<userTM> items = tblUser.getItems();
-                items.clear();
                 Button button = new Button("Remove");
-                button.setOnAction(event -> removeUserOnAction());
-
-                items.add(new userTM(rst.getString("username"),rst.getString("name"),
-                        rst.getString("password"),rst.getString("role"),button));
+                userTM userTM = new userTM(rst.getString("username"), rst.getString("name"),
+                        rst.getString("password"), rst.getString("role"), button);
+                items.add(userTM);
+                button.setOnAction(event -> removeUserOnAction(userTM));
 
             }
         } catch (SQLException e) {
@@ -74,18 +86,146 @@ public class ManageUsersFormController {
         }
     }
 
-    private void removeUserOnAction() {
+    private void removeUserOnAction(userTM tm) {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM user WHERE username=?");
+            stm.setString(1,tm.getUsername());
+            int i = stm.executeUpdate();
+            loadAllUsers();
+            if (i!=1){
+                throw new RuntimeException("Something went wrong! Please try again!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void btnUpdateRoleOnAction(ActionEvent actionEvent) {
+        disableControls(false);
+        btnUpdateRole.setDisable(true);
+        txtUsername.setDisable(true);
+        btnCreateRole.setText("Update");
     }
 
     public void btnCreateRoleOnAction(ActionEvent actionEvent) {
+        if (isValidated()){
+            if (btnCreateRole.getText().equals("Update")){
+                userTM sItem = tblUser.getSelectionModel().getSelectedItem();
+                sItem.setName(txtName.getText());
+                sItem.setUsername(txtUsername.getText());
+                sItem.setPassword(txtPassword.getText());
+                updateDatabase(sItem);
+                clearFields();
+                btnCreateRole.setText("Create Role");
+                disableControls(true);
+                tblUser.getSelectionModel().clearSelection();
+                btnAddNewUser.requestFocus();
+                loadAllUsers();
+
+            }else {
+                Optional<ButtonType> buttonType = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to add a new User?",ButtonType.NO,ButtonType.YES).showAndWait();
+                if (buttonType.get().equals(ButtonType.YES)){
+                    try {
+                        Connection connection = DBConnection.getInstance().getConnection();
+                        PreparedStatement stm = connection.prepareStatement("INSERT INTO user (username, name, password, role) VALUES (?,?,?,?)");
+                        stm.setString(1,txtUsername.getText());
+                        stm.setString(2,txtName.getText());
+                        stm.setString(3,txtPassword.getText());
+                        stm.setString(4,menuRole.getText());
+                        int i = stm.executeUpdate();
+                        if (i!=1){
+                            throw new RuntimeException("Failed to initialize the database");
+                        }
+                        loadAllUsers();
+                        clearFields();
+                    } catch (SQLException e) {
+                        new Alert(Alert.AlertType.ERROR,"Something went wrong! Please try again!",ButtonType.OK).show();
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private void updateDatabase(userTM sItem) {
+        String username = sItem.getUsername();
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement stm = connection.prepareStatement("UPDATE user SET name=?,password=?,role=? WHERE username=?");
+            stm.setString(1,txtName.getText());
+            stm.setString(2,txtPassword.getText());
+            stm.setString(3,menuRole.getText());
+            stm.setString(4,username);
+            int update = stm.executeUpdate();
+            if (update!=1){
+                throw new RuntimeException("Something went wrong! Please try again!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void btnAddNewUserOnAction(ActionEvent actionEvent) {
+        clearFields();
+        btnCreateRole.setText("Create Role");
+        tblUser.getSelectionModel().clearSelection();
         disableControls(false);
         txtUsername.requestFocus();
+        btnUpdateRole.setDisable(true);
+    }
+
+    private void clearFields() {
+        txtUsername.clear();
+        txtName.clear();
+        txtPassword.clear();
+        txtConfirmPassword.clear();
+        menuRole.setText("Select Role");
+    }
+
+    public boolean isValidated(){
+        String name = txtName.getText().trim();
+        String username = txtUsername.getText().trim();
+        String password = txtPassword.getText().trim();
+        String confirmPassword = txtConfirmPassword.getText().trim();
+
+        if (username.length() <4) {
+            new Alert(Alert.AlertType.ERROR, "Username should be at least 4 characters long").show();
+            txtUsername.selectAll();
+            txtUsername.requestFocus();
+            return false;
+        } else if (!name.matches("[A-Za-z ]+")) {
+            new Alert(Alert.AlertType.ERROR, "Please enter a valid name!").show();
+            txtName.selectAll();
+            txtName.requestFocus();
+            return false;
+        } else if (!username.matches("[A-Za-z0-9]+")) {
+            new Alert(Alert.AlertType.ERROR, "Username can contain only characters and digits").show();
+            txtUsername.selectAll();
+            txtUsername.requestFocus();
+            return false;
+        } else if (password.length() < 4) {
+            new Alert(Alert.AlertType.ERROR, "Password should be at least 4 characters long").show();
+            txtPassword.selectAll();
+            txtPassword.requestFocus();
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            new Alert(Alert.AlertType.ERROR, "Password mismatch").show();
+            txtConfirmPassword.selectAll();
+            txtConfirmPassword.requestFocus();
+            return false;
+        }else if(menuRole.getText().equals("Select a Role")){
+            new Alert(Alert.AlertType.ERROR,"Please select a user role",ButtonType.OK).show();
+            menuRole.requestFocus();
+            return false;
+        }else {
+            return true;
+        }
+
     }
 }
